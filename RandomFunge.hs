86b88fd389af99f4cac5e5b8a2cf7c8d3353e31d
shans <shane.stephens@gmail.com>
2012-11-08 22:23:30 +1100

    Random funge now prints seed for reproducible results

--- RandomFunge.hs
+++ RandomFunge.hs
@@ -34,15 +34,27 @@ loadRandomFunge' dim = do
   charSet <- config getCharSet
   extensions <- config getActiveExtensions
   let opSet = map (decodeInstruction (length dim)) (charSet ++ extensions)
+  seed <- io $ getStdGen
+  io $ putStrLn (show seed)
   m <- io $ randomFunge' opSet dim (replicate (length dim) 0) [] emptyMap
   putInstructionMap m
   putBounds (map (\a -> (0, a)) dim)
-  where
-    randomFunge' :: [Instruction] -> [Int] -> [Int] -> [Int] -> InstructionMap -> IO InstructionMap
-    randomFunge' _ [0] _ _ map = return map
-    randomFunge' ops [l] [pos] rest map = do {op <- randomOp ops; randomFunge' ops [l - 1] [pos + 1] rest $ updateNonEmpty (rest ++ [pos]) op map }
-    randomFunge' _ (0:t) _ _ map = return map
-    randomFunge' ops (h:t) (ph:pt) rest map = do { r <- randomFunge' ops t pt (ph:rest) map; randomFunge' ops ((h-1):t) ((ph+1):pt) rest r }
-    randomOp opSet = do
-      r <- randomIO
-      return $ opSet !! (r `mod` length opSet)
+    
+randomFunge' :: [Instruction] -> [Int] -> [Int] -> [Int] -> InstructionMap -> IO InstructionMap
+randomFunge' _ [0] _ _ map = return map
+randomFunge' ops [l] [pos] rest map = do {op <- randomOp ops; randomFunge' ops [l - 1] [pos + 1] rest $ updateNonEmpty (rest ++ [pos]) op map }
+randomFunge' _ (0:t) _ _ map = return map
+randomFunge' ops (h:t) (ph:pt) rest map = do { r <- randomFunge' ops t pt (ph:rest) map; randomFunge' ops ((h-1):t) ((ph+1):pt) rest r }
+randomOp opSet = do
+  r <- randomIO
+  return $ opSet !! (r `mod` length opSet)
+
+randomFungeFromSeed :: String -> [Int] -> VMRunner ()
+randomFungeFromSeed seed dim = do
+  charSet <- config getCharSet
+  extensions <- config getActiveExtensions
+  let opSet = map (decodeInstruction (length dim)) (charSet ++ extensions)
+  io $ setStdGen (read seed)
+  m <- io $ randomFunge' opSet dim (replicate (length dim) 0) [] emptyMap
+  putInstructionMap m
+  putBounds (map (\a -> (0, a)) dim)
